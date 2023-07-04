4960a8f3864f41e5ecadf30057fc1e24a8b346fe
Adeola Gbolahan <camtude2018@gmail.com>
2023-07-05 00:04:23 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
+import {setInterval} from "timers";
 
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -15,6 +17,7 @@ interface IState {
  * It renders title, button and Graph react element.
  */
 class App extends Component<{}, IState> {
+  private dataStreamInterval: NodeJS.Timeout | null = null;
   constructor(props: {}) {
     super(props);
 
@@ -22,6 +25,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,7 +33,9 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
@@ -37,10 +43,26 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      this.setState((prevState) => ({
+        data: [...prevState.data, ...serverResponds],
+      }));
     });
+  }
+
+  startDataStreaming() {
+    if (!this.dataStreamInterval) {
+      this.dataStreamInterval = setInterval(
+          () => this.getDataFromServer(),
+          100
+      );
+    }
+  }
+
+  stopDataStreaming (){
+    if (this.dataStreamInterval){
+      clearInterval(this.dataStreamInterval);
+      this.dataStreamInterval = null;
+    }
   }
 
   /**
@@ -68,6 +90,10 @@ class App extends Component<{}, IState> {
         </div>
       </div>
     )
+  }
+
+  componentWillUnmount() {
+    this.stopDataStreaming();
   }
 }
 
